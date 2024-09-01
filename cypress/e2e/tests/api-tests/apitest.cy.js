efb987b623e3c486b7c6ff37d66fd1f9af468625
Darya Lapitskaya <d.kov0809@gmail.com>
2024-09-01 23:11:07 +0300

    api draft

--- cypress/e2e/tests/api-tests/apitest.cy.js
+++
@@ -1,9 +0,0 @@
-describe("Users api testing", () => {
-    it("fetches users - GET", () => {
-      cy.request("/users?page=2").as("usersRequest");
-      cy.get("@usersRequest").then((users) => {
-        expect(users.status).to.eq(200);
-        assert.isArray(users.body.data, "Users Response is an array");
-      });
-    });
-  });
